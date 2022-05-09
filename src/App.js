b5e2185d36b49da08e2e9de3e696a3051eabdd4a
ana suryana <anasuryana10@gmail.com>
2022-05-09 14:52:08 +0700

    hasil coba coba

--- src/App.js
+++ src/App.js
@@ -3,12 +3,78 @@ import {Button, Container, Row, Col, InputGroup, FormControl,ButtonGroup,Dropdow
 import { Helmet } from 'react-helmet'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClipboard,faFileExport,faFileExcel } from '@fortawesome/free-solid-svg-icons'
-import { useState } from 'react'
+import { useRef } from 'react'
+import axios from 'axios'
+import numeral from 'numeral'
+
 function App() {
-  const [dateFrom, setdateFrom] = useState('')
-  function simpan(){
-    console.log('coba')
-    console.log(dateFrom)
+  const endpoint = process.env.REACT_APP_API_ENDPOINT+'/wms/SER/conversion_test'
+  const btnSearch = useRef()
+  const ajuDate = useRef()
+  const ajuDate2 = useRef()
+  const model = useRef()
+  const nomorAju = useRef()
+  const tblMain = useRef()  
+  function getData() {    
+    btnSearch.current.disabled = true
+    btnSearch.current.innerHTML = 'Please wait'
+    const tblMainBody = tblMain.current.getElementsByTagName('tbody')[0]
+    let newrow, newcell
+    tblMainBody.innerHTML = ''
+    axios.get(endpoint, {params: {
+      ajuDate: ajuDate.current.value
+      , model: model.current.value
+      , nomoraju: nomorAju.current.value
+      , ajuDate2: ajuDate2.current.value}}
+    ).then(res => {
+      btnSearch.current.disabled = false
+      btnSearch.current.innerHTML = 'Search'
+      for(let i=0;i<res.data.data.length; i++){        
+        newrow = tblMainBody.insertRow(-1)
+        newcell = newrow.insertCell(0)       
+        newcell.innerHTML = res.data.data[i].DLV_ZNOMOR_AJU
+        newcell = newrow.insertCell(1)
+        newcell.innerHTML = res.data.data[i].SER_ITMID
+        newcell = newrow.insertCell(2)
+        newcell.innerHTML = numeral(res.data.data[i].DLVQT).format(',')
+        newcell = newrow.insertCell(3)
+        newcell.innerHTML = res.data.data[i].SERD2_ITMCD
+        newcell = newrow.insertCell(4)
+        newcell.innerHTML = res.data.data[i].PARTDESCRIPTION
+        newcell = newrow.insertCell(5)
+        newcell.innerHTML = res.data.data[i].PER*1
+        newcell = newrow.insertCell(6)
+        newcell.innerHTML = numeral(res.data.data[i].RMQT).format(',')
+        newcell = newrow.insertCell(7)
+        newcell.innerHTML = res.data.data[i].PPSN1_BOMRV  
+      }
+    }).catch(function(error) {
+      btnSearch.current.disabled = false
+      btnSearch.current.innerHTML = 'Search'
+      console.log(error)
+    })
+    // for(let i=1;i<=10000;i++){
+    //   newrow = tblMainBody.insertRow(-1)
+    //   newcell = newrow.insertCell(0)
+    //   newcell.onclick = () => {
+    //     console.log('klik baris ke' +i)
+    //   }
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(1)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(2)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(3)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(4)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(5)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(6)
+    //   newcell.innerHTML = "baris " +i
+    //   newcell = newrow.insertCell(7)
+    //   newcell.innerHTML = "baris " +i
+    // }
   }
   return (
     <div style={{padding : 10}}>
@@ -20,32 +86,32 @@ function App() {
           <Col className='col-md-3 mb-1'>
             <InputGroup size="sm">
               <InputGroup.Text>Date [from]</InputGroup.Text>
-              <FormControl id='rconv_test_txt_dt' defaultValue={dateFrom} onChange={setdateFrom}/>
+              <FormControl ref={ajuDate}/>
             </InputGroup>
           </Col>
           <Col className='col-md-3 mb-1'>
             <InputGroup size="sm">
               <InputGroup.Text>Date [to]</InputGroup.Text>
-              <FormControl id='rconv_test_txt_dt2'/>
+              <FormControl ref={ajuDate2}/>
             </InputGroup>
           </Col>
           <Col className='col-md-3 mb-1'>
             <InputGroup size="sm">
               <InputGroup.Text>Nomor Aju</InputGroup.Text>
-              <FormControl id='rconv_test_txt_aju'/>
+              <FormControl ref={nomorAju}/>
             </InputGroup>
           </Col>
           <Col className='col-md-3 mb-1'>
             <InputGroup size="sm">
               <InputGroup.Text>Model Code</InputGroup.Text>
-              <FormControl id='rconv_test_txt_model'/>
+              <FormControl ref={model}/>
             </InputGroup>
           </Col>
         </Row>
         <Row>
           <Col className='col-md-12 mb-1'>
             <ButtonGroup size="sm">
-              <Button variant="primary" id="rconv_test_btn_gen" onClick={simpan}>Search</Button>
+              <Button variant="primary" onClick={getData} ref={btnSearch}>Search</Button>
               <DropdownButton variant="secondary" size="sm" as={ButtonGroup} title={<FontAwesomeIcon icon={faFileExport} />} id="bg-nested-dropdown">
                 <Dropdown.Item eventKey="1"><span style={{color: 'MediumSeaGreen'}}><FontAwesomeIcon icon={faFileExcel} /></span> Spreadsheet</Dropdown.Item>
                 <Dropdown.Item eventKey="2"><FontAwesomeIcon icon={faClipboard} /> Clipboard</Dropdown.Item>
@@ -55,7 +121,7 @@ function App() {
         </Row>
         <Row>
           <Col className='col-md-12 mb-1'>
-            <Table size='sm' responsive style={{fontSize: '75%'}} id="rconv_test_tbl">
+            <Table size='sm' responsive style={{fontSize: '75%'}} ref={tblMain}>
               <thead className='table-light'>
                 <tr>
                   <th  className="align-middle ">Nomor Aju</th>
@@ -68,6 +134,9 @@ function App() {
                   <th  className="align-middle text-center">Bom Revision</th>
                 </tr>
               </thead>
+              <tbody>
+
+              </tbody>
             </Table>
           </Col>
         </Row>
